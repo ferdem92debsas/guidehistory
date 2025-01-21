bc5ae907e030b266bef8d0d3758eb944ae5c26f2
ferdem92debsas <fernandocame1711@gmail.com>
2025-01-20 22:46:41 -0300

    first commit for this proyect

---
+++ noticia.js
@@ -0,0 +1,32 @@
+let link1= document.getElementById('note1');
+let dir1="https://www.profitablecpmrate.com/fb9uejhd?key=616262121f2ce0f81d123b2693cfd926";
+
+
+link1.addEventListener('click',function(e){
+	e.preventDefault();
+	window.open(dir1,'_blank');
+	let data1=document.getElementById('datanote');
+	data1.style.display = 'block';
+});
+
+let link2= document.getElementById('note2');
+let dir2="https://www.profitablecpmrate.com/cx30j1h09?key=7fd782d2a763a3a9549fc18a27872df6";
+
+
+link2.addEventListener('click',function(e){
+	e.preventDefault();
+	window.open(dir2,'_blank');
+	let data2=document.getElementById('datanote2');
+	data2.style.display = 'block';
+});
+
+let link3= document.getElementById('note3');
+let dir3="https://www.profitablecpmrate.com/ahtnu509ut?key=27fe0d9329e13917255e2b64e1996129";
+
+
+link3.addEventListener('click',function(e){
+	e.preventDefault();
+	window.open(dir3,'_blank');
+	let data3=document.getElementById('datanote3');
+	data3.style.display = 'block';
+});
